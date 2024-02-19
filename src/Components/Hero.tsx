ef0f60f9be72506f5f9548745267a443003672bd
Shahil <shahilyasarhaque@gmail.com>
2024-02-19 23:25:03 +0600

    Ui components booking selector, offer section, popular destination mostly done.

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -17,13 +17,13 @@ const Hero = () => {
               height={200}
             />
             <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center space-y-4">
-              <p className="text-white text-4xl font-bold md:text-6xl lg:text-8xl shadow-sm">
+              <p className="text-white text-4xl font-bold md:text-6xl lg:text-8xl shadow-sm drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                   <span style={{ color: "#E7FCFD" }}>Welcome to </span>
                   <span style={{ color: "#EE9B00" }}>go</span>
                   <span style={{ color: "#2C5768" }}>Travel</span>
                   <span style={{ color: "#EE9B00" }}>ot</span>
               </p>
-              <p className="text-[#E7FCFD] text-2xl font-bold md:text-3xl lg:text-4xl shadow-sm">
+              <p className="text-[#E7FCFD] text-2xl font-bold md:text-3xl lg:text-4xl shadow-sm drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                 The best place to find the best travel deals
               </p>
               <div className=" flex items-center justify-center ">
