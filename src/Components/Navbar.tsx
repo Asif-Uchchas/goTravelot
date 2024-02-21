0904217998321ac5fc577889892e4a9c09a50c45
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-02-21 21:44:56 +0600

    Updated Navbar design and added necessary buttons

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import CustomButton from './CustomButton'
-import DropdownButton from './DropdownButton'
-import { buttonVariants } from './ui/button'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import CustomButton from "./CustomButton";
+import DropdownButton from "./DropdownButton";
+import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <header className='w-full fixed top-0 z-10 bg-gradient-to-b from-amber-300 from-25%'>
+    <header className="w-full fixed top-0 z-10 bg-gradient-to-b from-amber-300 from-25%">
       <nav className="max-w-[2000px] mx-auto flex justify-between sm:px-16 px-6 py-4 ">
         <Link href="/" className="flex">
           <Image
@@ -18,18 +18,39 @@ const Navbar = () => {
             className="object-contain"
           />
         </Link>
-        
+
         {/* Wasi- Started Working from here */}
-        
-        
-        <div className='flex '>
-           <Link className={buttonVariants()} href='/sign-in'>Sign In</Link>
-           
-          <DropdownButton/>
+
+        <div className="">
+          <ul style={{ color: "#1f2937" }} className="hidden md:flex">
+            <Link href="/">
+              <CustomButton title="Home" containerStyles="nav-button" />
+            </Link>
+            <Link href="/#hotels">
+              <CustomButton title="Hotels" containerStyles="nav-button" />
+            </Link>
+            <Link href="/#places">
+              <CustomButton title="Places" containerStyles="nav-button" />
+            </Link>
+            <Link href="/#contact">
+              <CustomButton title="Contact" containerStyles="nav-button" />
+            </Link>
+            <Link href="/#faq">
+              <CustomButton title="FAQ" containerStyles="nav-button" />
+            </Link>
+            <Link href="/sign-in">
+              <CustomButton title="Sign In" containerStyles="nav-button" />
+            </Link>
+
+            <DropdownButton />
+            {/* <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div> */}
+          </ul>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/form/SignUpForm.tsx
+++ src/Components/form/SignUpForm.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../ui/input";
@@ -9,124 +16,145 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 import GoogleSignInButton from "../GoogleSignInButton";
 import { useRouter } from "next/navigation";
+import CustomButton from "../CustomButton";
 
-const FormSchema = z.object({
-    username: z.string().min(1,'Username is required').max(100),
-    email: z.string().min(1,'Email is required').email('Invalid email'),
-    password: z.
-    string()
-    .min(1,'Password is required')
-    .min(8,'Password must have minimum of 8 characters'),
-    confirmPassword: z.string().min(1,'Confirmation required'),
-}) .refine((data)=>data.password===data.confirmPassword,{
-    path:['confirmPassword'],
-    message: 'Passwords do not match',
-});
+const FormSchema = z
+  .object({
+    username: z.string().min(1, "Username is required").max(100),
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have minimum of 8 characters"),
+    confirmPassword: z.string().min(1, "Confirmation required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
 
 const SignUpForm = () => {
-    const router=useRouter();
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues:{
-            username: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-        },
-});
-
-const onSubmit= async (values:z.infer<typeof FormSchema>)=>{
-    const response=await fetch('/api/user',{
-    method:'POST',
-    headers:{
-        'Content-Type': 'application/json'
+  const router = useRouter();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     },
-    body:JSON.stringify({
-        username:values.username,
-        email:values.email,
-        password:values.password
-        })
-    })
-    if(response.ok){
-        router.push('/sign-in')
-    }
-    else{
-        console.error('Registration failed');
-    }
-};
+  });
 
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: values.username,
+        email: values.email,
+        password: values.password,
+      }),
+    });
+    if (response.ok) {
+      router.push("/sign-in");
+    } else {
+      console.error("Registration failed");
+    }
+  };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
         <div className="space-y-2">
-            <FormField
+          <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
-                <FormItem>
+              <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl>
-                    <Input placeholder="Enter your Username" {...field} />
+                  <Input placeholder="Enter your Username" {...field} />
                 </FormControl>
                 <FormMessage />
-                </FormItem>
+              </FormItem>
             )}
-            />
-            <FormField
+          />
+          <FormField
             control={form.control}
             name="email"
             render={({ field }) => (
-                <FormItem>
+              <FormItem>
                 <FormLabel>Email</FormLabel>
                 <FormControl>
-                    <Input placeholder="Enter your email" {...field} />
+                  <Input placeholder="Enter your email" {...field} />
                 </FormControl>
                 <FormMessage />
-                </FormItem>
+              </FormItem>
             )}
-            />
-            <FormField
+          />
+          <FormField
             control={form.control}
             name="password"
             render={({ field }) => (
-                <FormItem>
+              <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
-                    <Input placeholder="Enter your password" type="password" {...field} />
+                  <Input
+                    placeholder="Enter your password"
+                    type="password"
+                    {...field}
+                  />
                 </FormControl>
                 <FormMessage />
-                </FormItem>
+              </FormItem>
             )}
-            />
-            <FormField
+          />
+          <FormField
             control={form.control}
             name="confirmPassword"
             render={({ field }) => (
-                <FormItem>
+              <FormItem>
                 <FormLabel>Confirm Password</FormLabel>
                 <FormControl>
-                    <Input placeholder="Confirm your password" type="password" {...field} />
+                  <Input
+                    placeholder="Confirm your password"
+                    type="password"
+                    {...field}
+                  />
                 </FormControl>
                 <FormMessage />
-                </FormItem>
+              </FormItem>
             )}
-            />
+          />
         </div>
-        <Button className="w-full mt-6" type="submit">Sign Up</Button>
+
+        <CustomButton
+          title="Sign Up"
+          btnType="submit"
+          containerStyles="w-full mt-6"
+        />
       </form>
-      <div className="mx-auto my-4 flex w-full items-center justify-evenly
+      <div
+        className="mx-auto my-4 flex w-full items-center justify-evenly
       before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400
-      after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+      after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400"
+      >
         Or
       </div>
       <GoogleSignInButton>Sign up with Google</GoogleSignInButton>
       <p className="text-center text-sm text-gray-600 mt-2">
         If you have an account, please
-        <Link className="text-blue-500 hover:underline before:mr-1" href='/sign-in'>Sign In</Link>
+        <Link
+          className="text-blue-500 hover:underline before:mr-1"
+          href="/sign-in"
+        >
+          Sign In
+        </Link>
       </p>
     </Form>
-  )
-}
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
